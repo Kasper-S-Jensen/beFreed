594c88f7e9edea3b33d0baff6ad8573ea0c1d56e
Kasper-S-Jensen <kasperscoop@gmail.com>
2022-03-26 12:51:53 +0100

    implemented fragments and navigation manager instead of using main activity for view

--- app/src/main/java/io/github/KasperSJensen/beFreed/JournalOverviewFragment.kt
+++ app/src/main/java/io/github/KasperSJensen/beFreed/JournalOverviewFragment.kt
@@ -1,25 +1,31 @@
 package io.github.KasperSJensen.beFreed
 
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import android.widget.Toast
-import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
 
-class MainActivity : AppCompatActivity() {
+class JournalOverviewFragment : Fragment() {
 
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val view = inflater.inflate(R.layout.fragment_journal_overview, container, false)
 
-       /* val pokemonList: RecyclerView
-        val pokemonAdapter: CelebrityAdapter
+        val celebrityList: RecyclerView
+        val celebrityAdapter: CelebrityAdapter
 
-        pokemonList = findViewById(R.id.recyclerview)
-        pokemonList.hasFixedSize()
-        pokemonList.layoutManager= LinearLayoutManager(this)
+        celebrityList = view.findViewById(R.id.recyclerview)
+        celebrityList.hasFixedSize()
+        celebrityList.layoutManager= LinearLayoutManager(view.context)
 
         var celebrities = ArrayList<Celebrity>()
         celebrities.add(Celebrity("kenobi",R.drawable.obiwan))
@@ -42,17 +48,18 @@ class MainActivity : AppCompatActivity() {
         celebrities.add(Celebrity("bob",R.drawable.obiwan))
         celebrities.add(Celebrity("hans",R.drawable.obiwan))
         celebrities.add(Celebrity("kennet",R.drawable.obiwan))
-        pokemonAdapter = CelebrityAdapter(celebrities);
-        pokemonList.adapter=pokemonAdapter
+        celebrityAdapter = CelebrityAdapter(celebrities);
+        celebrityList.adapter=celebrityAdapter
 
-        pokemonAdapter.setOnClickListener { celebrity: Celebrity ->
+        celebrityAdapter.setOnClickListener { celebrity: Celebrity ->
             Toast.makeText(
-                this,
+                this.context,
                 celebrity.name,
                 Toast.LENGTH_SHORT
             ).show()
         }
-*/
+
+        return view
     }
 
 
---
+++ app/src/main/java/io/github/KasperSJensen/beFreed/ProfileFragment.kt
@@ -0,0 +1,30 @@
+package io.github.KasperSJensen.beFreed
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+
+class ProfileFragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+
+        val view = inflater.inflate(R.layout.fragment_profile, container, false)
+
+
+        return view
+
+    }
+
+
+}

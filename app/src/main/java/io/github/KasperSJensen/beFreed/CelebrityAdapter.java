8e967168a297f9b3d675227e8ef129d1e8eb059e
Kasper-S-Jensen <kasperscoop@gmail.com>
2022-03-26 10:27:53 +0100

    completed recyclerview with placeholder listitem

---
+++ app/src/main/java/io/github/KasperSJensen/beFreed/CelebrityAdapter.java
@@ -0,0 +1,54 @@
+package io.github.KasperSJensen.beFreed;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class CelebrityAdapter extends RecyclerView.Adapter<CelebrityAdapter.ViewHolder> {
+
+    private ArrayList<Celebrity> celebrities;
+
+    public CelebrityAdapter(ArrayList<Celebrity> celebrities) {
+        this.celebrities = celebrities;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+        View view = inflater.inflate(R.layout.celebrity_list_item, parent, false);
+        return new ViewHolder(view);
+
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        holder.name.setText(celebrities.get(position).getName());
+        holder.picture.setImageResource(celebrities.get(position).getPicture());
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return celebrities.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+        private final TextView name;
+        private final ImageView picture;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            name = itemView.findViewById(R.id.celebrityname);
+            picture = itemView.findViewById(R.id.celebritypic);
+        }
+    }
+}
--- app/src/main/java/io/github/KasperSJensen/beFreed/MainActivity.kt
+++ app/src/main/java/io/github/KasperSJensen/beFreed/MainActivity.kt
@@ -2,7 +2,9 @@ package io.github.KasperSJensen.beFreed
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import java.util.ArrayList
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,9 +14,35 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val pokemonList: RecyclerView
-        val pokemonAdapter: Pokemonadapter
+        val pokemonAdapter: CelebrityAdapter
 
+        pokemonList = findViewById(R.id.recyclerview)
+        pokemonList.hasFixedSize()
+        pokemonList.layoutManager= LinearLayoutManager(this)
 
+        var celebrities = ArrayList<Celebrity>()
+        celebrities.add(Celebrity("kenobi",R.drawable.obiwan))
+        celebrities.add(Celebrity("jens",R.drawable.obiwan))
+        celebrities.add(Celebrity("bob",R.drawable.obiwan))
+        celebrities.add(Celebrity("hans",R.drawable.obiwan))
+        celebrities.add(Celebrity("kennet",R.drawable.obiwan))
+        celebrities.add(Celebrity("kenobi",R.drawable.obiwan))
+        celebrities.add(Celebrity("jens",R.drawable.obiwan))
+        celebrities.add(Celebrity("bob",R.drawable.obiwan))
+        celebrities.add(Celebrity("hans",R.drawable.obiwan))
+        celebrities.add(Celebrity("kennet",R.drawable.obiwan))
+        celebrities.add(Celebrity("kenobi",R.drawable.obiwan))
+        celebrities.add(Celebrity("jens",R.drawable.obiwan))
+        celebrities.add(Celebrity("bob",R.drawable.obiwan))
+        celebrities.add(Celebrity("hans",R.drawable.obiwan))
+        celebrities.add(Celebrity("kennet",R.drawable.obiwan))
+        celebrities.add(Celebrity("kenobi",R.drawable.obiwan))
+        celebrities.add(Celebrity("jens",R.drawable.obiwan))
+        celebrities.add(Celebrity("bob",R.drawable.obiwan))
+        celebrities.add(Celebrity("hans",R.drawable.obiwan))
+        celebrities.add(Celebrity("kennet",R.drawable.obiwan))
+        pokemonAdapter = CelebrityAdapter(celebrities);
+        pokemonList.adapter=pokemonAdapter
     }
 
 

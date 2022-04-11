86cf75e82cea7c4b672554befc4cf5a49b8add35
Kasper-S-Jensen <kasperscoop@gmail.com>
2022-04-11 10:04:09 +0200

    added challenges fragment, and added directions to it

--- app/src/main/java/io/github/KasperSJensen/beFreed/ui/Home/HomeFragment.kt
+++ app/src/main/java/io/github/KasperSJensen/beFreed/ui/Home/HomeFragment.kt
@@ -5,7 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.cardview.widget.CardView
+import androidx.navigation.Navigation
 import io.github.KasperSJensen.beFreed.R
+import io.github.KasperSJensen.beFreed.ui.Journal.Overview.JournalOverviewFragmentDirections
 
 class HomeFragment : Fragment() {
 
@@ -14,7 +17,18 @@ class HomeFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        val view = inflater.inflate(R.layout.fragment_home, container, false)
+
+        val challengesButton = view.findViewById<CardView>(R.id.challengesCardView)
+
+        challengesButton.setOnClickListener {
+            val action =
+                HomeFragmentDirections.actionHomeFragmentToChallengesFragment()
+            Navigation.findNavController(view).navigate(action)
+        }
+
+
+        return view
     }
 
 }
